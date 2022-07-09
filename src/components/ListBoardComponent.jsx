a237a2afca303bf4210585c225ef994ec809080b
miroku93 <miroku93@gmail.com>
2022-07-10 03:57:14 +0900

    체크박스 복수 삭제 적용 자동갱신X

--- src/components/ListBoardComponent.jsx
+++ src/components/ListBoardComponent.jsx
@@ -10,25 +10,63 @@ class ListBoardComponent extends Component {
       p_num: 1,
       paging: {},
       boards: [],
+      checkLists: [],
     };
 
     this.createBoard = this.createBoard.bind(this);
+    this.deleteView = this.deleteView.bind(this);
+    this.changeHandler = this.changeHandler.bind(this);
   }
 
   componentDidMount() {
-    BoardService.getBoards().then((res) => {
-      this.setState({ boards: res.data });
-      // BoardService.getBoards(this.state.p_num).then((res) => {
-      //   console.log(res);
-      //   this.setState({
-      //     p_num: res.data.pagingData.currentPageNum,
-      //     paging: res.data.pagingData,
-      //     boards: res.data.list,
-      //   });
+    // BoardService.getBoards().then((res) => {
+    //   this.setState({ boards: res.data });
+    BoardService.getBoards(this.state.p_num).then((res) => {
+      console.log(res);
+      this.setState({
+        p_num: res.data.pagingData.currentPageNum,
+        paging: res.data.pagingData,
+        boards: res.data.list,
+      });
     });
   }
 
+  // changeHandler = (checked, id) => {
+  //   if (checked) {
+  //     this.setState([...this.state.checkLists, id]);
+  //   } else {
+  //     // 체크 해제
+  //     this.setState(this.state.checkLists.filter((el) => el !== id));
+  //   }
+  // };
+
+  changeHandler = (event, no) => {
+    // 체크할 시 CheckList에 id값 넣기
+    if (event.target.checked) {
+      console.log("event.target.checked IN----------:" + no);
+      //this.setState({ checkLists: no });
+      //this.setState([...this.state.checkLists, no]);
+      this.setState((state) => {
+        const checkLists = state.checkLists.concat(no);
+        return { checkLists };
+      });
+      console.log("this.state.checkLists:" + this.state.checkLists);
+    } else {
+      console.log("event.target.checked else----------");
+      // this.setState(
+      //   this.state.checkLists.filter((checkedId) => checkedId !== no)
+      // );
+      this.setState((state) => {
+        const checkLists = state.checkLists.filter(
+          (checkedId) => checkedId !== no
+        );
+        return { checkLists };
+      });
+    }
+  };
+
   createBoard() {
+    console.log("this.state.checkLists:" + this.state.checkLists);
     this.props.history.push("/create-board/_create");
   }
 
@@ -48,6 +86,35 @@ class ListBoardComponent extends Component {
       });
     });
     //console.log(this.state.boards);
+  }
+
+  deleteView(e) {
+    e.preventDefault();
+    e.persist();
+    if (
+      window.confirm(
+        "정말로 글을 삭제하시겠습니까?\n삭제된 글은 복구 할 수 없습니다."
+      )
+    ) {
+      let no = this.state.checkLists;
+
+      if (no.length > 0) {
+        console.log("delete no => " + no);
+        BoardService.deleteBoard(no).then((res) => {
+          //BoardService.deleteBoard(no).then((res) => {
+          console.log("delete result => " + JSON.stringify(res));
+          if (res.status === 200) {
+            this.props.history.push("/board");
+          } else {
+            alert("글 삭제가 실패했습니다.");
+          }
+          this.props.history.push("/board");
+        });
+      } else {
+        alert("글 삭제가 실패했습니다!!!!");
+        this.props.history.push("/board");
+      }
+    }
   }
 
   viewPaging() {
@@ -140,14 +207,21 @@ class ListBoardComponent extends Component {
         <h2 className="text-center">Boards List</h2>
         <div className="row">
           <button className="btn btn-primary" onClick={this.createBoard}>
-            {" "}
             글 작성
+          </button>
+          <button
+            className="btn btn-danger"
+            style={{ marginLeft: "10px" }}
+            onClick={this.deleteView}
+          >
+            삭제
           </button>
         </div>
         <div className="row">
           <table className="table table-striped table-bordered">
             <thead>
               <tr>
+                <th>선택</th>
                 <th>글 번호</th>
                 <th>타이틀 </th>
                 <th>작성자 </th>
@@ -161,6 +235,21 @@ class ListBoardComponent extends Component {
             <tbody>
               {this.state.boards.map((board) => (
                 <tr key={board.no}>
+                  <td>
+                    <input
+                      name={board.no}
+                      type="checkbox"
+                      className="checkbox ck"
+                      // onChange={(e) => {
+                      //   this.changeHandler(e.currentTarget.checked, board.no);
+                      // }}
+                      // checked={
+                      //   this.state.checkLists.filter(board.no) ? true : false
+                      // }
+                      onChange={(e) => this.changeHandler(e, board.no)}
+                      //checked={this.checkLists.includes(board)}
+                    />
+                  </td>
                   <td> {board.no} </td>
                   <td>
                     {" "}
